c4805e3eb9035781a7407b4fe556b466041b9506
jiovanniro <jiovanni.ro@gmail.com>
2020-12-04 18:23:13 -0500

    Create an event

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -8,13 +8,18 @@ import { sampleData } from '../../../app/api/sampleData';
 const EventsDashboard = ({formOpen, setFormOpen}) => {
     const [events, setEvents] = useState(sampleData);
 
+    const handleCreateEvent = (e) => {
+        console.log(e)
+        setEvents([...events, e])
+    }
+
     return (
         <Grid>
             <Grid.Column width={10}>
-                <EventLists events={sampleData}/>
+                <EventLists events={events}/>
             </Grid.Column>
             <Grid.Column width={6}>
-                {formOpen && <EventForm setFormOpen={setFormOpen} /> }
+                {formOpen && <EventForm setFormOpen={setFormOpen} setEvents={setEvents} createEvent={handleCreateEvent}/> }
             </Grid.Column>
         </Grid>
     )
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,28 +1,94 @@
-import React from "react";
+import cuid from "cuid";
+import React, { useState } from "react";
 import { Segment, Header, Form, Button } from "semantic-ui-react";
 
-const EventForm = ({ setFormOpen }) => {
+const EventForm = ({ setFormOpen, createEvent }) => {
+  const initialValues = {
+    title: "",
+    category: "",
+    description: "",
+    city: "",
+    venue: "",
+    date: "",
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  const handleFormSubmit = () => {
+    createEvent({
+      ...values,
+      id: cuid(),
+      hostedBy: 'Jose',
+      attendee: [],
+      hostPhotoURL: '/assets/user.png',
+    });
+  };
+
+  const handleInputChange = (e) => {
+    //get the name name and value from the target
+    const { name, value } = e.target;
+
+    //copy all of the current values and for the one that matches the name, update the value
+    setValues({ ...values, [name]: value });
+  };
+
   return (
     <Segment clearing>
       <Header content='Create new event' />
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <Form.Field>
-          <input type='text' placeholder='Event Title' />
+          <input
+            type='text'
+            name='title'
+            placeholder='Event Title'
+            value={values.title}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Category' />
+          <input
+            type='text'
+            name='category'
+            placeholder='Category'
+            value={values.category}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Description' />
+          <input
+            type='text'
+            name='description'
+            placeholder='Description'
+            value={values.description}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='City' />
+          <input
+            type='text'
+            name='city'
+            placeholder='City'
+            value={values.city}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Venue' />
+          <input
+            type='text'
+            name='venue'
+            placeholder='Venue'
+            value={values.venue}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='date' placeholder='Date' />
+          <input
+            type='date'
+            name='date'
+            placeholder='Date'
+            value={values.date}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Button type='submit' floated='right' positive content='Submit' />
         <Button
